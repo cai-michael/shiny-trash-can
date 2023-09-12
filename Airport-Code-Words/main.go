0538e26dcc324a26ad35aa8a3eda2439b94a3792
cai-michael <cai.michael.r@gmail.com>
2023-09-12 16:48:54 -0700

    Added Smarter way of doing things

--- Airport-Code-Words/main.go
+++ Airport-Code-Words/main.go
@@ -13,32 +13,27 @@ import (
 	"strings"
 )
 
-type TripleTuple struct {
-	a, b, c interface{}
-}
-type QuadTuple struct {
-	a, b, c, d interface{}
-}
 type QuintTuple struct {
 	a, b, c, d, e interface{}
 }
 
 func main() {
-	dictionaryURL := "https://raw.githubusercontent.com/zeisler/scrabble/master/db/dictionary.csv"
-	airportCodeURL := "https://raw.githubusercontent.com/datasets/airport-codes/master/data/airport-codes.csv"
-	dictionaryPath := download_csv(dictionaryURL)
-	airportCodePath := download_csv(airportCodeURL)
-	six_letter_words := filter_x_letter_words(dictionaryPath, 6)
-	nine_letter_words := filter_x_letter_words(dictionaryPath, 9)
-	twelve_letter_words := filter_x_letter_words(dictionaryPath, 12)
-	airport_codes := get_airport_codes(airportCodePath)
+	// dictionaryURL := "https://raw.githubusercontent.com/zeisler/scrabble/master/db/dictionary.csv"
+	// airportCodeURL := "https://raw.githubusercontent.com/datasets/airport-codes/master/data/airport-codes.csv"
+	// dictionaryPath := download_csv(dictionaryURL)
+	// airportCodePath := download_csv(airportCodeURL)
+	// six_letter_words := filter_x_letter_words(dictionaryPath, 6)
+	// nine_letter_words := filter_x_letter_words(dictionaryPath, 9)
+	// twelve_letter_words := filter_x_letter_words(dictionaryPath, 12)
+	// airport_codes := get_airport_codes(airportCodePath)
 
-	//six_letter_words := filter_x_letter_words("data/dictionary.csv", 6)
-	//nine_letter_words := filter_x_letter_words("data/dictionary.csv", 9)
-	//twelve_letter_words := filter_x_letter_words("data/dictionary.csv", 12)
-	//airport_codes := get_airport_codes("data/airport-codes.csv")
+	six_letter_words := filter_x_letter_words("data/dictionary.csv", 6)
+	nine_letter_words := filter_x_letter_words("data/dictionary.csv", 9)
+	twelve_letter_words := filter_x_letter_words("data/dictionary.csv", 12)
+	airport_codes := get_airport_codes("data/airport-codes.csv")
 
-	find_combinations_brute_force(six_letter_words, nine_letter_words, twelve_letter_words, airport_codes)
+	// find_combinations_brute_force(six_letter_words, nine_letter_words, twelve_letter_words, airport_codes)
+	find_combinations_prefixing(six_letter_words, nine_letter_words, twelve_letter_words, airport_codes)
 
 }
 
@@ -166,22 +161,22 @@ func get_airport_codes(filePath string) []string {
 }
 
 func find_combinations_brute_force(six_letter_words []string, nine_letter_words []string, twelve_letter_words []string, airport_codes []string) {
-	created_six_letter_words := []TripleTuple{}
-	created_nine_letter_words := []QuadTuple{}
+	created_six_letter_words := []QuintTuple{}
+	created_nine_letter_words := []QuintTuple{}
 	created_twelve_letter_words := []QuintTuple{}
 	for _, code_one := range airport_codes {
 		for _, code_two := range airport_codes {
 			concatenated_code_one := code_one + code_two
 			for _, word := range six_letter_words {
 				if strings.EqualFold(concatenated_code_one, word) {
-					created_six_letter_words = append(created_six_letter_words, TripleTuple{concatenated_code_one, code_one, code_two})
+					created_six_letter_words = append(created_six_letter_words, QuintTuple{concatenated_code_one, code_one, code_two, "", ""})
 				}
 			}
 			for _, code_three := range airport_codes {
 				concatenated_code_two := concatenated_code_one + code_three
 				for _, word := range nine_letter_words {
 					if strings.EqualFold(concatenated_code_two, word) {
-						created_nine_letter_words = append(created_nine_letter_words, QuadTuple{concatenated_code_two, code_one, code_two, code_three})
+						created_nine_letter_words = append(created_nine_letter_words, QuintTuple{concatenated_code_two, code_one, code_two, code_three, ""})
 					}
 				}
 
@@ -198,33 +193,94 @@ func find_combinations_brute_force(six_letter_words []string, nine_letter_words 
 		}
 	}
 
+	output_results_described(created_six_letter_words, created_nine_letter_words, created_twelve_letter_words)
+}
+
+func find_combinations_prefixing(six_letter_words []string, nine_letter_words []string, twelve_letter_words []string, airport_codes []string) {
+
+	six_matches := []string{}
+	nine_matches := []string{}
+	twelve_matches := []string{}
+
+	three_prefix_matches_for_six := []string{}
+	three_prefix_matches_for_nine := []string{}
+	three_prefix_matches_for_twelve := []string{}
+	for _, code_one := range airport_codes {
+		three_prefix_matches_for_six = append(three_prefix_matches_for_six, get_infix_matches(six_letter_words, code_one, 3, 0)...)
+		three_prefix_matches_for_nine = append(three_prefix_matches_for_nine, get_infix_matches(nine_letter_words, code_one, 3, 0)...)
+		three_prefix_matches_for_twelve = append(three_prefix_matches_for_twelve, get_infix_matches(twelve_letter_words, code_one, 3, 0)...)
+	}
+
+	six_prefix_matches_for_nine := []string{}
+	six_prefix_matches_for_twelve := []string{}
+	for _, code_two := range airport_codes {
+		six_matches = append(six_matches, get_infix_matches(three_prefix_matches_for_six, code_two, 3, 3)...)
+		six_prefix_matches_for_nine = append(six_prefix_matches_for_nine, get_infix_matches(three_prefix_matches_for_nine, code_two, 3, 3)...)
+		six_prefix_matches_for_twelve = append(six_prefix_matches_for_twelve, get_infix_matches(three_prefix_matches_for_twelve, code_two, 3, 3)...)
+	}
+
+	nine_prefix_matches_for_twelve := []string{}
+	for _, code_three := range airport_codes {
+		nine_matches = append(nine_matches, get_infix_matches(six_prefix_matches_for_nine, code_three, 3, 6)...)
+		nine_prefix_matches_for_twelve = append(nine_prefix_matches_for_twelve, get_infix_matches(six_prefix_matches_for_twelve, code_three, 3, 6)...)
+	}
+
+	for _, code_three := range airport_codes {
+		twelve_matches = append(twelve_matches, get_infix_matches(nine_prefix_matches_for_twelve, code_three, 3, 9)...)
+	}
+
+	output_results(six_matches, nine_matches, twelve_matches)
+}
+
+func get_infix_matches(words []string, prefix string, prefix_length int, skip int) []string {
+	matches := []string{}
+	for _, word := range words {
+		word_prefix := n_letters(word, prefix_length, skip)
+		if strings.EqualFold(prefix, word_prefix) {
+			matches = append(matches, word)
+		}
+	}
+
+	return matches
+}
+
+func output_results_described(results_six []QuintTuple, results_nine []QuintTuple, results_twelve []QuintTuple) {
 	to_write_six := []string{}
 	to_write_nine := []string{}
 	to_write_twelve := []string{}
 
-	for _, tuple := range created_six_letter_words {
+	for _, tuple := range results_six {
 		combination_code := fmt.Sprintf("%s + %s = %s\n", tuple.b, tuple.c, tuple.a)
 		to_write_six = append(to_write_six, combination_code)
 		fmt.Printf("%s + %s = %s\n", tuple.b, tuple.c, tuple.a)
 	}
-	writeLines(to_write_six, "data/results_6.txt")
+	write_lines(to_write_six, "results_six.txt")
 
-	for _, tuple := range created_nine_letter_words {
+	for _, tuple := range results_nine {
 		combination_code := fmt.Sprintf("%s + %s + %s = %s\n", tuple.b, tuple.c, tuple.d, tuple.a)
 		to_write_nine = append(to_write_nine, combination_code)
 		fmt.Printf("%s + %s + %s = %s\n", tuple.b, tuple.c, tuple.d, tuple.a)
 	}
-	writeLines(to_write_six, "data/results_9.txt")
+	write_lines(to_write_nine, "results_nine.txt")
 
-	for _, tuple := range created_twelve_letter_words {
+	for _, tuple := range results_twelve {
 		combination_code := fmt.Sprintf("%s + %s + %s + %s = %s\n", tuple.b, tuple.c, tuple.d, tuple.e, tuple.a)
 		to_write_twelve = append(to_write_twelve, combination_code)
 		fmt.Printf("%s + %s + %s + %s = %s\n", tuple.b, tuple.c, tuple.d, tuple.e, tuple.a)
 	}
-	writeLines(to_write_six, "data/results_12.txt")
+	write_lines(to_write_twelve, "results_twelve.txt")
 }
 
-func writeLines(lines []string, path string) error {
+func output_results(results_six []string, results_nine []string, results_twelve []string) {
+
+	write_lines(results_six, "results_six.txt")
+
+	write_lines(results_nine, "results_nine.txt")
+
+	write_lines(results_twelve, "results_twelve.txt")
+}
+
+func write_lines(lines []string, path string) error {
 	file, err := os.Create(path)
 	if err != nil {
 		return err
@@ -237,4 +293,15 @@ func writeLines(lines []string, path string) error {
 	}
 
 	return w.Flush()
+}
+
+func n_letters(s string, n int, k int) string {
+	i := 0
+	for j := range s {
+		if i == (n + k) {
+			return s[k:j]
+		}
+		i++
+	}
+	return s[k:]
 }
